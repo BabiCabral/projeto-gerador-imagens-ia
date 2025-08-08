1439698a6e6e5631a4c3a07db71d90db5ad66f07
Bárbara Cabral <cabral.brm@gmail.com>
2025-08-08 19:14:52 -0300

    build(deps): adiciona as bibliotecas Flask e Flask-CORS

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from openai import OpenAI
 from dotenv import load_dotenv
 import requests
@@ -6,17 +8,17 @@ load_dotenv()
 
 cliente = OpenAI()
 
-resposta = cliente.images.generate(
-    model="dall-e-3",
-    prompt="uma imagem realista de orixás e entidades da umbanda sendo representados por vários gatinhos vestidos e adornados.",
-    size="1024x1024"
-)
+# resposta = cliente.images.generate(
+#     model="dall-e-3",
+#     prompt="uma imagem realista de varios gatinhos vestidos e adornados.",
+#     size="1024x1024"
+# )
 
-url_imagem = resposta.data[0].url
-print("Imagem gerada com sucesso!")
-print("Link: ", url_imagem)
+# url_imagem = resposta.data[0].url
+# print("Imagem gerada com sucesso!")
+# print("Link: ", url_imagem)
 
-informacoes_imagem = requests.get(url_imagem)
+# informacoes_imagem = requests.get(url_imagem)
 
-with open("imagem.jpg", "wb") as arquivo_imagem:
-    arquivo_imagem.write(informacoes_imagem.content)
+# with open("imagem.jpg", "wb") as arquivo_imagem:
+#     arquivo_imagem.write(informacoes_imagem.content)
