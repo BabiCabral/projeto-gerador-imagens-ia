96adc0180635439448b9e6eeeec4c5b589a49bc3
Bárbara Cabral <cabral.brm@gmail.com>
2025-08-08 18:47:44 -0300

    feat(image): adiciona lógica inicial para gerar imagem com DALL-E
    Este commit implementa o código inicial para gerar imagens usando o modelo DALL-E 3 da OpenAI. O código salva a imagem gerada em um arquivo local. Esta é uma implementação de teste e sofrerá alterações futuras.

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,22 @@
 from openai import OpenAI
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
 cliente = OpenAI()
+
+resposta = cliente.images.generate(
+    model="dall-e-3",
+    prompt="uma imagem realista de orixás e entidades da umbanda sendo representados por vários gatinhos vestidos e adornados.",
+    size="1024x1024"
+)
+
+url_imagem = resposta.data[0].url
+print("Imagem gerada com sucesso!")
+print("Link: ", url_imagem)
+
+informacoes_imagem = requests.get(url_imagem)
+
+with open("imagem.jpg", "wb") as arquivo_imagem:
+    arquivo_imagem.write(informacoes_imagem.content)
